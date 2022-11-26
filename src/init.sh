4a7eb365bbc19227ea4ce1f30136bd716fb974e9
ITY <110728147+ityspace@users.noreply.github.com>
2022-11-27 02:43:25 +0800

    Update init.sh

--- src/init.sh
+++ src/init.sh
@@ -4,9 +4,6 @@ test -d public && echo "\033[31mError: public directory is exist!\033[0m" && exi
 
 test -d posts && echo "\033[31mError: posts directory is exist!\033[0m" && exit 0
 
-mkdir public
-mkdir posts
-
 read -p "Domain (https://example.com): " Domain
 read -p "Title: " Title
 read -p "Description: " Description
@@ -16,6 +13,9 @@ echo "DOMAIN=\"$Domain\"" > config
 echo "TITLE=\"$Title\"" >> config
 echo "DESCRIPTION=\"$Description\"" >> config
 echo "AUTHOR=\"$Author\"" >> config
+
+mkdir public
+mkdir posts
 
 cp ~/.config/terb/src/rss.xsl public/rss.xsl
 
