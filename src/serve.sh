f8945070426db98ac684b2bcc5fb841cdfa4c929
ITY <110728147+ityspace@users.noreply.github.com>
2022-11-27 01:39:03 +0800

    Update serve.sh

--- src/serve.sh
+++ src/serve.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+bin="$PREFIX/bin"
+
+if [ ! -f "${bin}/python3" ]; then
+    echo -e "${error}Please installed the python3 before you start !"
+    exit 0
+fi
+
 bash ~/.config/terb/src/build.sh
 python3 -m http.server --directory ./public/
